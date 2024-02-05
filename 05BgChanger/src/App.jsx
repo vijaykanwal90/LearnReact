febdfb7a3d539f9f4649434e803e7e48882f07a4
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-02-05 18:27:08 +0530

    bgchanger done

--- 05BgChanger/src/App.jsx
+++ 05BgChanger/src/App.jsx
@@ -1,12 +1,26 @@
-
+import { useState } from "react"
 
 function App() {
 
+  const [color, setColor] = useState("pink")
 
   return (
-    <>
-    
-    </>
+
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+      <div className=" fixed flex flex-wrap  justify-center  bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4" style={{ backgroundColor: "red" }} onClick={() => setColor("red")}>Red</button>
+          <button className="outline-none px-4" style={{ backgroundColor: "green" }} onClick={() => setColor("green")}>Green</button>
+          <button className="outline-none px-4" style={{ backgroundColor: "blue" }} onClick={() => setColor("blue")}>Blue</button>
+          <button className="outline-none px-4" style={{ backgroundColor: "white" }} onClick={() => setColor("white")}>White</button>
+          <button className="outline-none px-4" style={{ backgroundColor: "black" }} onClick={() => setColor("black")}>Black</button>
+          <button className="outline-none px-4" style={{ backgroundColor: "olive" }} onClick={() => setColor("olive")}>Olive</button>
+        </div>
+      </div>
+
+    </div>
+
+
   )
 }
 
