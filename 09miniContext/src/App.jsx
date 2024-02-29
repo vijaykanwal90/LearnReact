69ca31536ae61f5f614d73a9c40c801b8128080f
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-02-29 21:43:27 +0530

    context api done

--- 09miniContext/src/App.jsx
+++ 09miniContext/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+
 import UserContext from './context/UserContext'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <UserContextProvider>
     <h1>React context api</h1>
+    <Login/>
+    <Profile/>
     </UserContextProvider>
   )
 }
--- 09miniContext/src/components/Profile.jsx
+++ 09miniContext/src/components/Profile.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React ,{useContext}from 'react'
+import UserContext from '../context/UserContext'
 const Profile = () => {
+  const {user} = useContext(UserContext)
+
+  if(!user) return <div>Please login</div>
   return (
-    <div>Profile</div>
+
+
+    <div>Welcome {user.username}</div>
   )
 }
 
