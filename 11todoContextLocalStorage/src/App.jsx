78b7d147f710364d00835cf41ef3b6af7570b4ab
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-03-12 00:17:28 +0530

    context api done

--- 11todoContextLocalStorage/src/App.jsx
+++ 11todoContextLocalStorage/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './context'
+import { TodoForm , TodoItem } from './components'
 
 function App() {
   const [todos , setTodos] = useState([])
@@ -19,8 +20,8 @@ function App() {
     setTodos((prev)=> prev.filter((todo)=>todo.id !==id))
   }
 
-  const toggleComplete=()=>{
-    setTodos((prev)=>prev.map((prevTodo)=> prevTodo ===id? {...prevTodo , completed:!prevTodo.completed}:prevTodo))
+  const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=> prevTodo.id ===id? {...prevTodo , completed:!prevTodo.completed}:prevTodo))
   }
  
   useEffect(()=>{
@@ -44,9 +45,15 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id} className='w-full'>
+<TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
